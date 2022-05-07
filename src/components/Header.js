a5c03fe0db474c487d789aeb3a592c730515d790
Anup adhikary <becr794@gmail.com>
2022-05-08 02:26:44 +0545

    1st commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="navbar">
+        
+            <h2 className="logo">Meme Generator</h2>
+        </header>
+    )
+}
